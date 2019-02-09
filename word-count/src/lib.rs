e292330c90b09b913bdd55f8a37a0899f3bc0a18
fish <mikail.khan45@gmail.com>
2019-02-09 17:41:56 -0500

    incomplete word count

---
+++ word-count/src/lib.rs
@@ -0,0 +1,27 @@
+use std::collections::HashMap;
+
+/// Count occurrences of words.
+pub fn word_count(words: &str) -> HashMap<String, u32> {
+    let mut map: HashMap<String, u32> = HashMap::new();
+    let mut filtered: String = words.to_lowercase();
+    filtered.retain(|c| c != '\"' && c != ':');
+    println!("{}", filtered);
+
+    filtered.split(|c| c == ' ' || c == '\n' || c == ',')
+            .for_each(|word| {
+                let mut word: String = word.to_string();
+                word.retain(|c| c.is_alphanumeric() || c == '\'');
+
+
+                if word != "" {
+                    if map.contains_key(&word) {
+                        *map.get_mut(&word).unwrap() += 1; //get_mut returns an Option, Option.unwrap() returns the value
+                    } else {
+                        map.insert(word.to_string(), 1u32);
+                    }
+                }
+            });
+
+    println!("{:?}", map);
+    map
+}
