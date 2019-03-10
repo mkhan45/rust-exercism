3dfdf0d04210bf85d4f9a8fc88056f56d2ca1453
fish <mikail.khan45@gmail.com>
2019-03-10 13:50:11 -0400

    improved fib

--- fib/src/main.rs
+++ fib/src/main.rs
@@ -2,21 +2,21 @@ use std::io;
 use fib::*;
 
 fn main() {
-    // loop{
-    //     println!("Input:");
+    loop{
+        println!("Input:");
 
-    //     let mut input = String::new();
+        let mut input = String::new();
 
-    //     io::stdin().read_line(&mut input)
-    //         .expect("Failed to read line");
+        io::stdin().read_line(&mut input)
+            .expect("Failed to read line");
 
-    //     let input: usize = match input.trim().parse(){
-    //         Ok(num) => num,
-    //         Err(_) => continue,
-    //     };
+        let input: usize = match input.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    //     println!("Fib: {}", fib(input));
-    // }
-    
-    println!("{}", table_neatprint(multip_table(12)));
+        println!("Multiplication table: {}", table_neatprint(multip_table(input)));
+        println!("Fib: {}", fib(input));
+    }
+
 }
