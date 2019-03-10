887c9c6db4ff82aaeb050948f11657cf538de897
fish <mikail.khan45@gmail.com>
2019-03-10 13:47:05 -0400

    multiplication table in fib

--- fib/src/lib.rs
+++ fib/src/lib.rs
@@ -7,3 +7,29 @@ pub fn fib(n: usize) -> usize{
 
     return fib_arr[n-1];
 }
+
+pub fn multip_table(n: usize) -> Vec<Vec<usize>> {
+    let mut table = Vec::new();
+    for y in 1..=n{
+        let mut row = Vec::new();
+        for x in 0..=n{
+            row.push(x*y);
+        }
+        table.push(row);
+    }
+    return table;
+}
+
+pub fn table_neatprint<T: std::string::ToString>(table: Vec<Vec<T>>) -> String{
+    table.iter()
+        .fold("".to_string(), |acc: String, row| {
+            format!("{} \n {}", acc,
+                    row.iter()
+                    .fold("".to_string(),
+                          |rowacc, element: &T|{
+                              format!("{}\t{}", rowacc, element.to_string())
+                          }
+                         )
+                   )
+        })
+}
--- fib/src/main.rs
+++ fib/src/main.rs
@@ -1,20 +1,22 @@
 use std::io;
-use fib::fib;
+use fib::*;
 
 fn main() {
-    loop{
-        println!("Input:");
+    // loop{
+    //     println!("Input:");
 
-        let mut input = String::new();
+    //     let mut input = String::new();
 
-        io::stdin().read_line(&mut input)
-            .expect("Failed to read line");
+    //     io::stdin().read_line(&mut input)
+    //         .expect("Failed to read line");
 
-        let input: usize = match input.trim().parse(){
-            Ok(num) => num,
-            Err(_) => continue,
-        };
+    //     let input: usize = match input.trim().parse(){
+    //         Ok(num) => num,
+    //         Err(_) => continue,
+    //     };
 
-        println!("Fib: {}", fib(input));
-    }
+    //     println!("Fib: {}", fib(input));
+    // }
+    
+    println!("{}", table_neatprint(multip_table(12)));
 }
